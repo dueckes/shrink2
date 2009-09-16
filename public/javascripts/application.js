e9d94bad46bd27284590b5cd779a8f5ebd12abf0
Dueckes <mueckerman@DUECKES.(none)>
2009-09-17 02:09:38 +1000

    MU: Started integration of stylesheet and scripts developed by Pam.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function menutoggle(id) {
+	menuObject =  $(id);
+
+	if (menuObject.className.indexOf("current") == -1) {
+		menuObject.className = "current";
+	} else {
+		menuObject.className = "";
+	}
+}
+
+function showImportForm() {
+	Effect.toggle("importform", "blind");	
+	menutoggle("menu-import");
+}
