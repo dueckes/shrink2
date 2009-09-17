d98fb13b1b23d56cbb892ec90ee1a702f36dbb9a
Dueckes <mueckerman@DUECKES.(none)>
2009-09-18 00:34:16 +1000

    MU: Import Feature UI enhancements.

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -47,16 +47,22 @@ class FeaturesController < ApplicationController
 
   def import
     responds_to_parent do
-      feature_file = params[:feature_file]
-      destination_file_name = "#{Platter::Feature::UPLOAD_DIRECTORY}/#{feature_file.original_filename}"
-      File.open(destination_file_name, "w") { |file| file.write(feature_file.read) }
-      @feature = Platter::Cucumber::FeatureImporter.import_file(destination_file_name)
+      @feature = Platter::Cucumber::FeatureImporter.import_file(write_feature_file(params[:feature_file]))
       @feature.package = Platter::Package.find(params[:package_id])
       @feature.save!
       render(:update) do |page|
         page.insert_html(:bottom, :features, :partial => "features/show_shrunk_with_div", :locals => { :feature => @feature })
+        page.visual_effect(:toggle_blind, "importform")
+        page["feature_file"].clear
       end
     end
+  end
+
+  private
+  def write_feature_file(uploaded_feature_file)
+    destination_file_name = "#{Platter::Feature::UPLOAD_DIRECTORY}/#{uploaded_feature_file.original_filename}"
+    File.open(destination_file_name, "w") { |file| file.write(uploaded_feature_file.read) }
+    destination_file_name
   end
 
 end
