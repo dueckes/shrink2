7186ddbc95e5c331c06b92a4dc28a083c997e4d8
Dueckes <mueckerman@DUECKES.(none)>
2009-09-17 13:33:41 +1000

    MU: Continued integrating Pam's UI chanages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  # Similiar to div_for but produces an li tag
+  def li_for(record, *args, &block)
+    content_tag_for(:li, record, *args, &block)
+  end
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function menutoggle(id) {
+function toggleStyle(id) {
+	titleObj = id + "-title";
+
+	titleObject = $(titleObj);
+	if (titleObject.className.indexOf("current") == -1) {
+		titleObject.className = "current";
+	} else {
+		titleObject.className = "";
+	}
+}
+
+function toggle(id) {
+	Effect.toggle(id, "blind");
+	toggleStyle(id);
+}
+
+function menuToggle(id) {
 	menuObject =  $(id);
 
 	if (menuObject.className.indexOf("current") == -1) {
@@ -12,6 +28,6 @@ function menutoggle(id) {
 }
 
 function showImportForm() {
-	Effect.toggle("importform", "blind");	
-	menutoggle("menu-import");
+	Effect.toggle("importform", "blind");
+	menuToggle("menu-import");
 }
