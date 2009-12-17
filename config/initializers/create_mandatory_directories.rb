5b38271ee61354ca7d6da2ee0764e7f63d188232
Dueckes <mueckerman@DUECKES.(none)>
2009-12-18 01:14:36 +1100

    MU: Folders exportable.

--- config/initializers/create_mandatory_directories.rb
+++
@@ -1,3 +0,0 @@
-[Platter::Feature::IMPORT_DIRECTORY, Platter::Feature::EXPORT_DIRECTORY, Platter::Folder::EXPORT_DIRECTORY].each do |directory|
-  FileUtils.mkdir_p(directory)
-end
---
+++ spec/lib/extensions/gems/rubyzip/lib/zip/zip_integration_spec.rb
@@ -0,0 +1,12 @@
+describe ::Platter::Zip::ZipFile do
+
+  class TestableZipFile
+    include ::Platter::Zip::ZipFile
+  end
+
+  context "#extract" do
+
+    it "should extract a zip file "
+  end
+
+end
