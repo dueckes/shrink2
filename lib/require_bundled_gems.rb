186b8f8d7c00af3f03a827babd1d6a73dc913ec2
dueckes <matthew@ueckerman.net>
2010-09-05 19:04:40 +0200

    MU: Upgrade to Bundler 1.0.0

--- lib/require_bundled_gems.rb
+++ lib/require_bundled_gems.rb
@@ -1,7 +1,7 @@
 require "rubygems"
 require "bundler"
 
-raise "Bundler version 0.9.26 required." unless Bundler::VERSION == "0.9.26"
+raise "Bundler version 1.0.0 required." unless Bundler::VERSION == "1.0.0"
 
 ENV["BUNDLE_GEMFILE"] = File.expand_path(File.dirname(__FILE__) + "/../Gemfile")
 Bundler.setup
